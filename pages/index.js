cc1f584359d65acdd763e4d86fec267a5d579c1f
TommmyKelly <tommy_kelly@icloud.com>
2021-04-21 00:18:32 +0100

    Added staticProps and staticPaths for single event page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
 import EventItem from "@/components/EventItem";
+import Link from "next/link";
 
 export default function Home({ events }) {
   return (
@@ -11,6 +12,11 @@ export default function Home({ events }) {
       {events.map((evt) => (
         <EventItem key={evt.id} evt={evt} />
       ))}
+      {events.length > 0 && (
+        <Link href='/events'>
+          <a className='btn-secondary'>View all events</a>
+        </Link>
+      )}
     </Layout>
   );
 }
@@ -20,7 +26,7 @@ export async function getStaticProps() {
   const events = await res.json();
 
   return {
-    props: { events },
+    props: { events: events.slice(0, 3) },
     revalidate: 1,
   };
 }
